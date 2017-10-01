61a1aff0c827dbc01ed604007aa33d0a1e94991b
astur <astur@yandex.ru>
2017-10-01 15:54:54 +0300

    ofl.validObject test

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ test('Base API', t => {
             'flatten',
             'unflatten',
             'validKeys',
+            'validObject',
         ].sort(),
     );
 });
@@ -62,5 +63,50 @@ test('ofl.validKeys', t => {
     t.notThrows(
         () => ofl.validKeys(['a', 'a.b', 'c/d'], '/'),
         'delimiter',
+    );
+});
+
+test('ofl.validObject', t => {
+    t.notThrows(
+        () => ofl.validObject({a: 1, b: true, c: null, d: ''}),
+        'all scalars',
+    );
+    t.notThrows(
+        () => ofl.validObject({a: [1, true, null, '']}),
+        'array of scalars',
+    );
+    t.throws(
+        () => ofl.validObject({a: 1, 'a.b': 2}),
+        'Can not add field to scalar key: a',
+        'no fields added to scalar key',
+    );
+    t.notThrows(
+        () => ofl.validObject({a: 1, 'a.b': 1, 'c/d': 1}, '/'),
+        'delimiter',
+    );
+    t.throws(
+        () => ofl.validObject({a: undefined}),
+        'Invalid value type in field a',
+        'no undefined fields',
+    );
+    t.throws(
+        () => ofl.validObject({a: new Date()}),
+        'Invalid value type in field a',
+        'no undefined fields',
+    );
+    t.throws(
+        () => ofl.validObject({a: [{}]}),
+        'Invalid value type in field a',
+        'no object in arrays',
+    );
+    t.throws(
+        () => ofl.validObject({a: [[]]}),
+        'Invalid value type in field a',
+        'no nested arrays',
+    );
+    t.throws(
+        () => ofl.validObject({a: [undefined]}),
+        'Invalid value type in field a',
+        'no undefined in arrays',
     );
 });
